d82ca36c868f0cef063196fa8748cc8743530204
jadeph1010 <phamthaovan1010@gmail.com>
2026-06-06 20:05:17 +0700

    home.js

--- po18x.vip/src/home.js
+++ po18x.vip/src/home.js
@@ -1,10 +1,10 @@
 load('config.js');
 function execute() {
     return Response.success([
-        { title: "Biên Tập Đề Cử (编辑推荐)", script: "rank.js", input: "https://po18x.vip" },
-        { title: "Bảng Cất Chứa (收藏榜)", script: "rank.js", input: "https://po18x.vip" },
-        { title: "Tổng Click (总点击)", script: "rank.js", input: "https://po18x.vip" },
-        { title: "Tháng Click (月点击)", script: "rank.js", input: "https://po18x.vip" },
-        { title: "Tuần Click (周点击)", script: "rank.js", input: "https://po18x.vip" }
+        { title: "Biên Tập Đề Cử (编辑推荐)", script: "https://raw.githubusercontent.com/jadeph1010/vbook-extensions/refs/heads/main/po18x.vip/plugin/src/rank.js", input: "https://po18x.vip" },
+        { title: "Bảng Cất Chứa (收藏榜)", script: "https://raw.githubusercontent.com/jadeph1010/vbook-extensions/refs/heads/main/po18x.vip/plugin/src/rank.js", input: "https://po18x.vip" },
+        { title: "Tổng Click (总点击)", script: "https://raw.githubusercontent.com/jadeph1010/vbook-extensions/refs/heads/main/po18x.vip/plugin/src/rank.js", input: "https://po18x.vip" },
+        { title: "Tháng Click (月点击)", script: "https://raw.githubusercontent.com/jadeph1010/vbook-extensions/refs/heads/main/po18x.vip/plugin/src/rank.js", input: "https://po18x.vip" },
+        { title: "Tuần Click (周点击)", script: "https://raw.githubusercontent.com/jadeph1010/vbook-extensions/refs/heads/main/po18x.vip/plugin/src/rank.js", input: "https://po18x.vip" }
     ]);
 }
