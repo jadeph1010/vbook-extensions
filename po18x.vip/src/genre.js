3a065a8694fb5ce3fd4497d8380ba2e39ef79396
jadeph1010 <phamthaovan1010@gmail.com>
2026-06-06 17:05:14 +0700

    Add files via upload

---
+++ po18x.vip/src/genre.js
@@ -0,0 +1,23 @@
+function execute() {
+    const BASE_URL = "https://po18x.vip";
+    return Response.success([
+        { title: "Đô thị (都市)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/1-1.html" },
+        { title: "Nùng tình (浓情)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/2-1.html" },
+        { title: "Ngôn tình (言情)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/3-1.html" },
+        { title: "Vườn trường (校园)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/4-1.html" },
+        { title: "Võ hiệp (武侠)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/5-1.html" },
+        { title: "Huyền huyễn (玄幻)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/6-1.html" },
+        { title: "Xuyên qua (穿越)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/7-1.html" },
+        { title: "Kinh tủng (惊悚)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/8-1.html" },
+        { title: "Huyền nghi (悬疑)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/9-1.html" },
+        { title: "Trọng sinh (重生)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/10-1.html" },
+        { title: "Lịch sử (历史)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/11-1.html" },
+        { title: "Võng du (网游)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/12-1.html" },
+        { title: "Viễn tưởng (科幻)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/13-1.html" },
+        { title: "Đam mỹ (耽美)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/14-1.html" },
+        { title: "Cao cán (高干)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/15-1.html" },
+        { title: "Làm ruộng (种田)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/16-1.html" },
+        { title: "Bách hợp (百合)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/17-1.html" },
+        { title: "Khác (其他)", script: "https://githubusercontent.com", input: BASE_URL + "/sort/18-1.html" }
+    ]);
+}
---
+++ po18x.vip/src/home.js
@@ -0,0 +1,11 @@
+function execute() {
+    const BASE_URL = "https://po18x.vip";
+    return Response.success([
+        { title: "Biên Tập Đề Cử (编辑推荐)", script: "rank.js", input: "https://po18x.vip" },
+        { title: "Bảng Cất Chứa (收藏榜)", script: "rank.js", input: "https://po18x.vip" },
+        { title: "Tổng Click (总点击)", script: "rank.js", input: "https://po18x.vip" },
+        { title: "Tháng Click (月点击)", script: "rank.js", input: "https://po18x.vip" },
+        { title: "Tuần Click (周点击)", script: "rank.js", input: "https://po18x.vip" }
+    ]);
+}
+
